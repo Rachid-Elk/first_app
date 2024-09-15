d90305876ee649d9de7d9332dee62b71bb4d7cda
Rachid-Elk <rachidkhtr@gmail.com>
2024-09-15 21:58:58 +0100

    brache allo

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Branche 4</h1>
-      <h1>Branche </h1>
+      
+      <h1>Allo </h1>
       <h2> Courses 4 Arab</h2>
     </div>
   );
